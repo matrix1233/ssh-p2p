1654cac8d378d1b5aafb3e2b87ceebc865b238ff
matrix1233 <matrix1233@users.noreply.github.com>
2020-03-20 20:19:58 +0100

    Update schema.go

--- signaling/schema.go
+++ signaling/schema.go
@@ -1,7 +1,7 @@
 package signaling
 
 // URI default signaling server
-const URI = "https://stun.l.google.com"
+const URI = "https://stun.l.google.com:19302"
 
 // ConnectInfo SDP by offer or answer
 type ConnectInfo struct {
